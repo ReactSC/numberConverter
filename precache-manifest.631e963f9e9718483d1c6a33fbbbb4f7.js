2f26a8ee06188594ee677cb106fd850b30bc0e50
Sofiqul Islam <schoolwab@gmail.com>
2020-03-03 21:15:05 +0600

    Updates

---
+++ precache-manifest.631e963f9e9718483d1c6a33fbbbb4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf336078100cdc5efe92d2b679e678c",
+    "url": "/numberConverter/index.html"
+  },
+  {
+    "revision": "79d6ef887d2a91ed46b3",
+    "url": "/numberConverter/static/css/main.b9d2d36b.chunk.css"
+  },
+  {
+    "revision": "db8df337f5288a0b8247",
+    "url": "/numberConverter/static/js/2.47cc06dd.chunk.js"
+  },
+  {
+    "revision": "8f8edb6bab1da46340074408917ef17a",
+    "url": "/numberConverter/static/js/2.47cc06dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d6ef887d2a91ed46b3",
+    "url": "/numberConverter/static/js/main.6a02c8fb.chunk.js"
+  },
+  {
+    "revision": "4dad4b82bb79ab564b3b",
+    "url": "/numberConverter/static/js/runtime-main.9bc5a99c.js"
+  }
+]);
